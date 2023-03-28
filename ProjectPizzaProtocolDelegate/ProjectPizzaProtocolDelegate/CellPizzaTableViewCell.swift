7053eb508099381875992bbf2ea03bcf09aabaf8
Mirian Santana <miriansantana93@gmail.com>
2023-03-28 12:15:19 -0300

    Added class CellPizzaTableViewCell

---
+++ ProjectPizzaProtocolDelegate/ProjectPizzaProtocolDelegate/CellPizzaTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CellPizzaTableViewCell.swift
+//  ProjectPizzaProtocolDelegate
+//
+//  Created by Mirian Santana on 28/03/23.
+//
+
+import UIKit
+import SDWebImage
+
+class CellPizzaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imagePizzaCell: UIImageView!
+    @IBOutlet weak var lblPizzaCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
