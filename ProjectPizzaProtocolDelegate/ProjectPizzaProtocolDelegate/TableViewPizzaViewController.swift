879c85bc2f20e508525cbe8a6196f51f6ea1cd5b
Mirian Santana <miriansantana93@gmail.com>
2023-03-28 11:37:29 -0300

    Connection of button viewController to navigation. Added class TableViewPizzaViewController

---
+++ ProjectPizzaProtocolDelegate/ProjectPizzaProtocolDelegate/TableViewPizzaViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TableViewPizzaViewController.swift
+//  ProjectPizzaProtocolDelegate
+//
+//  Created by Mirian Santana on 28/03/23.
+//
+
+import UIKit
+
+class TableViewPizzaViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
--- ProjectPizzaProtocolDelegate/ProjectPizzaProtocolDelegate/ViewController.swift
+++ ProjectPizzaProtocolDelegate/ProjectPizzaProtocolDelegate/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btStart(_ sender: Any) {
+        if let screen = self.storyboard?.instantiateViewController(withIdentifier: "navigation") {
+            
+            screen.present(screen, animated: true)
+        }
+    }
+    
 }
 
